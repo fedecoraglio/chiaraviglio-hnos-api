c9f51e11abc52de69076f365a006190215698f28
fedecoraglio <federicocoraglio@gmail.com>
2021-01-14 10:31:53 -0300

    Update production.ts

--- src/env/production.ts
+++ src/env/production.ts
@@ -2,25 +2,25 @@
 
 export const envConfig: any = {
     database: {
-        DB_URI: 'chiarahnos.com.ar',
-        DB_PORT: 3306,
-        DB_USER: 'chiarahn_api',
-        DB_PASS: 'V3Ep~J_Ekc=}',
-        DB_MAIN: 'chiarahn_chiara_hnos'
+        DB_URI: '',
+        DB_PORT: ,
+        DB_USER: '',
+        DB_PASS: '',
+        DB_MAIN: ''
     },
-    secretKey: "AC61E5D9696B543E1B139FFF37E064E3",
+    secretKey: "",
     emailData: { //juan.chiara@chiarahnos.com.ar // Ckq,Mw=V$lum //diego.chiara@chiarahnos.com.ar //d)y-XgsEIMWz
-      user: 'sistemas@chiarahnos.com.ar', //'chiarahnosweb@gmail.com', //
-      pass: 'R{lb6)UDN=&5', //UVrXbA=+%cX7
-      from: 'sistemas@chiarahnos.com.ar',
-      service: 'mail.chiarahnos.com.ar',
+      user: '', //'chiarahnosweb@gmail.com', //
+      pass: '', //UVrXbA=+%cX7
+      from: '',
+      service: '',
     },
     ftpserver: {
-      host: 'chiarahnos.com.ar',
+      host: '',
       port: 21,
-      user: 'reportes@api.chiarahnos.com.ar',
-      password: 'hcaM?K-wgE*j',
-      logging: 'basic',
-      filePath: '/data'
+      user: '',
+      password: '',
+      logging: '',
+      filePath: ''
     }
 };
