770d276ea6215ad9c9dd1a9e13d086dc9ba0b8f2
fedecoraglio <federicocoraglio@gmail.com>
2021-01-14 10:34:05 -0300

    Update defaults.ts

--- src/env/defaults.ts
+++ src/env/defaults.ts
@@ -2,18 +2,18 @@
 
 const config: any = {
     port: process.env.PORT || 61000,
-    env: 'development',
+    env: '',
     database: {
-        client: 'mysql'
+        client: ''
     },
-    secretKey: "esta es una secret key no muy segura",
+    secretKey: "",
     ftpserver: {
-      host: 'estudiodellarossa.atwebpages.com',
-      port: 21,
-      user: '1711021',
-      password: 'example987654321',
-      logging: 'basic',
-      filePath: '/estudiodellarossa.atwebpages.com/test'
+      host: '',
+      port: ,
+      user: '',
+      password: '',
+      logging: '',
+      filePath: ''
     }
 };
 
